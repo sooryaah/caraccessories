92fffb02a30affa2073bb72d43a979a71ebe4338
Nandakishore-pr <nandakishore.p.r2002@gmail.com>
2025-06-05 15:14:38 +0530

    added authentication function and model to store user

--- backend/ecommerce/accounts/models.py
+++ backend/ecommerce/accounts/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+    is_admin_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'phone_number']
+
+    def __str__(self):
+        return self.email
--- backend/ecommerce/accounts/serializers.py
+++ backend/ecommerce/accounts/serializers.py
@@ -1,2 +1,42 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, OutstandingToken
+from rest_framework_simplejwt.tokens import RefreshToken
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password', 'phone_number')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=validated_data['password'],
+            phone_number=validated_data['phone_number']
+        )
+        return user
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username')
+
+        
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+    def validate(self, attrs):
+        self.token = attrs['refresh']
+        return attrs
+
+    def save(self, **kwargs):
+        try:
+            token = RefreshToken(self.token)
+            token.blacklist()
+        except Exception:
+            raise serializers.ValidationError('Token is invalid or expired.')
