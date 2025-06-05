80292135d9500ee2b75e919b661ec675462b4ed6
Nandakishore-pr <nandakishore.p.r2002@gmail.com>
2025-06-05 15:23:43 +0530

    changed db to postgresql

--- backend/ecommerce/ecommerce/settings.py
+++ backend/ecommerce/ecommerce/settings.py
@@ -87,23 +87,23 @@ CACHES = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'ecommerce_db',  # Replace with your database name
-#         'USER': 'postgres',      # Replace with your database username
-#         'PASSWORD': 'postgres1234',  # Replace with your database password
-#         'HOST': '192.168.29.84',   # Or the IP address of your PostgreSQL server
-#         'PORT': '5432',        # Default PostgreSQL port
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce_db',  # Replace with your database name
+        'USER': 'postgres',      # Replace with your database username
+        'PASSWORD': 'postgres1234',  # Replace with your database password
+        'HOST': '192.168.29.84',   # Or the IP address of your PostgreSQL server
+        'PORT': '5432',        # Default PostgreSQL port
+    }
+}
 
 
 
