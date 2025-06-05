ea4f35c1caee40c780cdcbcfb4a3052dc5f203f0
Nandakishore-pr <nandakishore.p.r2002@gmail.com>
2025-06-05 15:21:59 +0530

    corrected the model to inherit from abstract user

--- backend/ecommerce/accounts/models.py
+++ backend/ecommerce/accounts/models.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=15)
